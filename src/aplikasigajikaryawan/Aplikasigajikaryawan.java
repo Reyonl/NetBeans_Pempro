172fa62b11fa85cb7307d992a0d14038fead935a
Reyonl <liurey55@gmail.com>
2025-04-19 21:47:00 +0700

    add

---
+++ src/aplikasigajikaryawan/Aplikasigajikaryawan.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasigajikaryawan;
+
+import view.FormUtama;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Aplikasigajikaryawan {
+    private final static FormUtama formUtama = new FormUtama();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        formUtama.setVisible(true);
+    }
+    
+}
